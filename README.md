3da0bf86a722f7e77d0035f1d201205be6efc05d
Davor Šercer <davor.sercer@gmail.com>
2025-05-11 01:28:12 +0200

    hello world 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rust_by_example
+# Rust by Example
+
 Rust by Example (RBE) is a collection of runnable examples that illustrate various Rust concepts and standard libraries.
+
+[Learn Rust by Example](https://doc.rust-lang.org/rust-by-example/) – Get started with Rust
---
+++ a_hello_world/hello.rs
@@ -0,0 +1,32 @@
+// Line comments which go to the end of the line.
+/* Block comments which go to the closing delimiter. */
+
+/// Generate library docs for the following item.
+//! Generate library docs for the enclosing item.
+
+fn main() {
+    println!("Hello World!");
+    println!("I'm a Rustacean")
+
+    // This is an example of a line comment.
+    // There are two slashes at the beginning of the line.
+    // And nothing written after these will be read by the compiler.
+
+    // println!("Hello, world!");
+
+    // Run it. See? Now try deleting the two slashes, and run it again.
+
+    /*
+     * This is another type of comment, a block comment. In general,
+     * line comments are the recommended comment style. But block comments
+     * are extremely useful for temporarily disabling chunks of code.
+     * /* Block comments can be /* nested, */ */ so it takes only a few
+     * keystrokes to comment out everything in this main() function.
+     * /*/*/* Try it yourself! */*/*/
+     */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+}
