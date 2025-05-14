832741710908a7b166dfcfcab9555005e1f9c635
Davor Šercer <davor.sercer@gmail.com>
2025-05-14 12:29:20 +0200

    updated formatting

--- a_hello_world/formatting.rs
+++ a_hello_world/formatting.rs
@@ -39,7 +39,7 @@ impl Display for Color {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
             f,
-            "RGB ({0}, {1}, {2}) 0x{0:02X} {1:02X}{2:02X}",
+            "RGB ({0}, {1}, {2}) 0x{0:02X}{1:02X}{2:02X}",
             self.red, self.green, self.blue
         )
     }
